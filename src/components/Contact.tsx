85e8078fa4ff1f0cfe281a8915c803d257e90c51
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2024-12-23 14:40:16 +0100

    feat : modify resume

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -29,7 +29,7 @@ export default function Contact() {
 		{
 			icon: <FileText size={24} />,
 			text: language === "fr" ? "CV" : "Resume",
-			url: "/public/docs/cv2024FoucaultMathieuFullstackWebDev.pdf",
+			url: "/public/docs/cv-2024-Foucault-Mathieu-Developpeur-Web.pdf",
 			download: true,
 		},
 	];
